cd4de8f3dda61e13bd25ce56b0f327b175c1dc58
Simon Ask Ulsnes <simon@ulsnes.dk>
2009-09-27 23:35:28 +0200

    put Color in the namespace

--- color.h
+++ color.h
@@ -12,15 +12,26 @@
 
 #include "float4.h"
 
-typedef struct {
+namespace wings {
+
+struct Color {
 	float r, g, b, a;
-} Color;
+	
+	Color(float _r, float _g, float _b, float _a = 1.0f) : r(_r), g(_g), b(_b), a(_a) {}
+	Color(byte _r, byte _g, byte _b, byte _a = 255) : r((float)_r/255), g((float)_g/255), b((float)_b/255), a((float)_a/255) {}
+	
+	Color& operator=(const Color& c) {
+		r = c.r; g = c.g; b = c.b; a = c.a;
+		return *this;
+	}
+};
 
-static const Color COLOR_WHITE  = { 1.0f, 1.0f, 1.0f, 1.0f };
-static const Color COLOR_BLUE   = { 0.0f, 0.0f, 1.0f, 1.0f };
-static const Color COLOR_GREEN  = { 0.0f, 1.0f, 0.0f, 1.0f };
-static const Color COLOR_RED    = { 1.0f, 0.0f, 0.0f, 1.0f };
-static const Color COLOR_YELLOW = { 1.0f, 1.0f, 0.0f, 1.0f };
-
+static const Color COLOR_BLACK    ( 0.0f, 0.0f, 0.0f, 1.0f );
+static const Color COLOR_WHITE    ( 1.0f, 1.0f, 1.0f, 1.0f );
+static const Color COLOR_BLUE     ( 0.0f, 0.0f, 1.0f, 1.0f );
+static const Color COLOR_GREEN    ( 0.0f, 1.0f, 0.0f, 1.0f );
+static const Color COLOR_RED      ( 1.0f, 0.0f, 0.0f, 1.0f );
+static const Color COLOR_YELLOW   ( 1.0f, 1.0f, 0.0f, 1.0f );
+}
 
 #endif // _COLOR_H_
