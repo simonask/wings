3ab53cfcb7e38032c44a777848db482b7e7496dd
Simon Ask Ulsnes <simon@ulsnes.dk>
2009-09-27 23:37:35 +0200

    stub Entity class

---
+++ entity.cpp
@@ -0,0 +1,11 @@
+/*
+ *  entity.cpp
+ *  falling
+ *
+ *  Created by Simon Ask Ulsnes on 26/09/09.
+ *  Copyright 2009 __MyCompanyName__. All rights reserved.
+ *
+ */
+
+#include "entity.h"
+
---
+++ entity.h
@@ -0,0 +1,22 @@
+/*
+ *  entity.h
+ *  falling
+ *
+ *  Created by Simon Ask Ulsnes on 26/09/09.
+ *  Copyright 2009 __MyCompanyName__. All rights reserved.
+ *
+ */
+
+#ifndef _WINGS_ENTITY_H_
+#define _WINGS_ENTITY_H_
+
+
+namespace wings {
+	class Entity {
+	private:
+		
+	};
+}
+
+
+#endif
